56e6a8f32b76f7f435ab905ce4b621c46b16fdc2
pedrohsfontoura <pedrohenrisantosfontoura@gmail.com>
2021-08-20 13:07:58 -0300

    :sparkles: now the jobs list is random

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import FilterList from "./infra/layout/FilterList";
 import JobsList from "./infra/layout/JobsList";
 
 const App = () => {
-	const jobsList = jobs;
+	const jobsList = randomJobsList();
 	const [filterList, setFilterList] = useState([]);
+
+	function randomJobsList() {
+		const newJobs = jobs.filter((job) => job.new);
+		const notNewJobs = jobs.filter((job) => !job.new);
+
+		return [...newJobs, ...notNewJobs.sort(() => 0.5 - Math.random())];
+	}
 
 	return (
 		<>
